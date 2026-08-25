da3163433521fe319ac3a7ba084abf80a936bc9b
Cain-Cabrera <caincito.cabrera7@gmail.com>
2026-08-25 15:56:27 +0100

    add: Etapa 1 del proyecto final intragrador, preparamos estructura de carpetas y readme completo.

---
+++ backend/src/main/java/com/descansobygi/DescansobygiApplication.java
@@ -0,0 +1,13 @@
+package com.descansobygi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DescansobygiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DescansobygiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/descansobygi/DescansobygiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descansobygi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescansobygiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
